d6e026698c415e02d17160cadaea683666af316f
Aleksei <sinitsyn@MacBook-Pro-Aleksei.local>
2026-07-29 00:07:45 +0300

    feat: create appkit application

--- src/main.mm
+++ src/main.mm
@@ -1,1 +1,28 @@
-int main() { return 0; }
+#import <AppKit/AppKit.h>
+
+@interface BrivibaAppDelegate : NSObject <NSApplicationDelegate>
+@end
+
+@implementation BrivibaAppDelegate
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication*)sender {
+  (void)sender;
+  return YES;
+}
+
+@end
+
+int main(int argc, char* argv[]) {
+  (void)argc;
+  (void)argv;
+
+  @autoreleasepool {
+    NSApplication* application = [NSApplication sharedApplication];
+    BrivibaAppDelegate* delegate = [[BrivibaAppDelegate alloc] init];
+    [application setDelegate:delegate];
+    [application setActivationPolicy:NSApplicationActivationPolicyRegular];
+    [application run];
+  }
+
+  return 0;
+}
